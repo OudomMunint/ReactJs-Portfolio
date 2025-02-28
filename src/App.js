c55b03c301313e4dbb601f67d12d5110509eab99
OudomMunint <oudommunint@gmail.com>
2025-03-01 00:40:07 +1100

    Code clean up, re-enabled footer component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
@@ -7,6 +6,7 @@ import Projects from "./components/Projects/Projects";
 import ContactForm from "./components/Contact";
 import PdfView from "./components/PdfView";
 import ScrollToTop from "./components/ScrollToTop";
+import Footer from "./components/Footer";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,23 +16,11 @@ import {
   Routes,
   Navigate
 } from "react-router-dom";
-// import Footer from "./components/Footer";
 
 function App() {
-  const [load, updateLoad] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      updateLoad(false);
-    }, 1200);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <Router>
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <div className="App" >
         <Navbar />
         <ScrollToTop />
         <Routes>
@@ -43,7 +31,7 @@ function App() {
           <Route path="/resume" element={<PdfView />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </Router>
   );
