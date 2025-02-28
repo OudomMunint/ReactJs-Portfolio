39fd49323917a8d6f82bd3bbd8c502c8e0d297ad
OudomMunint <oudommunint@gmail.com>
2025-03-01 00:40:45 +1100

    Handle loading for PdfViewer component

--- src/components/PdfView.js
+++ src/components/PdfView.js
@@ -1,43 +1,56 @@
-import { Document, pdfjs, Page } from 'react-pdf';
-import resume from '../Assets/resume.pdf'
-import { Button } from 'react-bootstrap';
-import { CgSoftwareDownload } from 'react-icons/cg';
+import { Document, pdfjs, Page } from "react-pdf";
+import resume from "../Assets/resume.pdf";
+import { Button } from "react-bootstrap";
+import { CgSoftwareDownload } from "react-icons/cg";
+import { useState } from "react";
+import Preloader from "./Pre";
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 function PdfView() {
-    return (
-        <div className="resume-container">
-            <Document file={resume}>
-                <Page pageNumber={1} />
-            </Document>
+  const [load, updateLoad] = useState(true);
 
-            <Button variant="primary flex" download href={resume} 
-                    style={{ display: "flex", alignItems: "center", borderRadius: "0px"}}>
-                <CgSoftwareDownload size={50} />
-            </Button>
+  return (
+    <div className="resume-container">
+      <Preloader load={load} />
 
-            <style>
-                {`
-                .resume-container {
-                margin-top: calc(100% - 60px);
-                display: flex;
-                justify-content: center;
-                max-height: 800px;
-                overflow-y: auto;
-                margin-top: 80px;
-                }
+      <Document className="pdfdoc"
+                file={resume}
+                onLoadSuccess={() => updateLoad(false)}
+                onLoadError={(error) => { console.error("PDF Load Error:", error); updateLoad(false);}}>
+        <Page pageNumber={1} />
+      </Document>
 
-                div.react-pdf__Page__textContent.textLayer {
-                display: none !important;
-                }
+      <Button variant="primary flex"
+              download
+              href={resume}
+              style={{ display: "flex", alignItems: "center", borderRadius: "0px",}}>
+        <CgSoftwareDownload size={50} />
+      </Button>
 
-                div.react-pdf__Page__annotations.annotationLayer{
-                display: none !important;
-                }
-                    `}
-            </style>
+      <style>
+        {`
+          .resume-container {
+            margin-top: 80px;
+            display: flex;
+            justify-content: center;
+            max-height: 800px;
+            overflow-y: auto;
+          }
 
-        </div>
-    );
+          div.react-pdf__Page__textContent.textLayer {
+            display: none !important;
+          }
+
+          div.react-pdf__Page__annotations.annotationLayer {
+            display: none !important;
+          }
+        `}
+      </style>
+    </div>
+  );
 }
+
 export default PdfView;
